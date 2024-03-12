5daa0ebb7ce708f03ac5a70259238adaabd8f4e2
tiagoforward <devtiagoforward@gmail.com>
2024-03-12 11:47:37 -0300

    adicionando lógica do exercicio 4

--- exercicio-4.js
+++ exercicio-4.js
@@ -1,3 +1,5 @@
 // 4) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada.
 
 // Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+
+//Ao apertar o primeiro interruptor, na primeira ida, iremos ir verificar qual lâmpada acendeu na sala, digamos que acendeu a terceira lâmpada, então voltamos para o segundo interruptor, e na segunda ida verificamos qual lâmpada acendeu, digamos que dessa vez acendeu a primeira lâmpada, ao retornar aos interruptores, logo já saberemos qual lâmpada do terceiro interruptor irá acender, no caso a do meio.
